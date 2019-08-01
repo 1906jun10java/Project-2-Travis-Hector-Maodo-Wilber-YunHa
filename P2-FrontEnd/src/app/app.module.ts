bdeffca5f31d22523c2561b2f7a84339900d836e
Yun Ha Jung <jungyunha1994@live.com>
2019-08-01 16:44:32 -0400

    changes

--- P2-FrontEnd/src/app/app.module.ts
+++ P2-FrontEnd/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
