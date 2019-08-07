a5013afacd0110a6f5d7195218942a1bf4440043
travywill <travywill@gmail.com>
2019-08-07 19:31:14 -0400

    updates Address bean

--- p2/src/main/java/com/revature/beans/Address.java
+++ p2/src/main/java/com/revature/beans/Address.java
@@ -25,15 +25,6 @@ public class Address {
 	@Column(name="ADDRESS_LINE_1")
 	private String addressLine1;
 	
-	@Column(name="ADDRESS_LINE_2")
-	private String addressLine2;
-	
-	@Column(name="ADDRESS_CITY")
-	private String city;
-	
-	@Column(name="ADDRESS_STATE")
-	private String state;
-	
 	@Column(name="ADDRESS_ZIP_CODE")
 	private String zipCode;
 	
@@ -45,35 +36,24 @@ public class Address {
 		super();
 	}
 
-	public Address(int addressId, String addressLine1, String addressLine2, String city, String state, String zipCode,
-			User user) {
+	public Address(int addressId, String addressLine1, String zipCode, User user) {
 		super();
 		this.addressId = addressId;
 		this.addressLine1 = addressLine1;
-		this.addressLine2 = addressLine2;
-		this.city = city;
-		this.state = state;
+		this.zipCode = zipCode;
+		this.user = user;
+	}
+
+	public Address(String addressLine1, String zipCode, User user) {
+		super();
+		this.addressLine1 = addressLine1;
 		this.zipCode = zipCode;
 		this.user = user;
 	}
 	
-	public Address(String addressLine1, String addressLine2, String city, String state, String zipCode,
-			User user) {
+	public Address(String addressLine1, String zipCode) {
 		super();
 		this.addressLine1 = addressLine1;
-		this.addressLine2 = addressLine2;
-		this.city = city;
-		this.state = state;
-		this.zipCode = zipCode;
-		this.user = user;
-	}
-	
-	public Address(String addressLine1, String addressLine2, String city, String state, String zipCode) {
-		super();
-		this.addressLine1 = addressLine1;
-		this.addressLine2 = addressLine2;
-		this.city = city;
-		this.state = state;
 		this.zipCode = zipCode;
 	}
 
@@ -91,30 +71,6 @@ public class Address {
 
 	public void setAddressLine1(String addressLine1) {
 		this.addressLine1 = addressLine1;
-	}
-
-	public String getAddressLine2() {
-		return addressLine2;
-	}
-
-	public void setAddressLine2(String addressLine2) {
-		this.addressLine2 = addressLine2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
 	}
 
 	public String getZipCode() {
@@ -135,8 +91,8 @@ public class Address {
 
 	@Override
 	public String toString() {
-		return "Address [addressId=" + addressId + ", addressLine1=" + addressLine1 + ", addressLine2=" + addressLine2
-				+ ", city=" + city + ", state=" + state + ", zipCode=" + zipCode + ", user=" + user + "]";
+		return "Address [addressId=" + addressId + ", addressLine1=" + addressLine1 + ", zipCode=" + zipCode + ", user="
+				+ user + "]";
 	}
 	
 }
