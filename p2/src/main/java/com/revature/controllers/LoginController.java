8895580340de6f5c12fb9ef79b86bfbf85dd552d
wforce91 <wilbernoel@hotmail.com>
2019-08-08 19:17:00 -0400

    cross origin on controllers

--- p2/src/main/java/com/revature/controllers/LoginController.java
+++ p2/src/main/java/com/revature/controllers/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.revature.beans.Credentials;
 import com.revature.beans.User;
 import com.revature.services.LoginService;;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping(value="/login")
 public class LoginController {
@@ -28,11 +30,13 @@ private LoginService loginService;
 		this.loginService = loginService;
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> getAllUsers() {
 		return new ResponseEntity<>(loginService.getAllUsers(), HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@ResponseStatus(HttpStatus.OK)
 	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public User getCredentials(@RequestBody Credentials credentials) {
