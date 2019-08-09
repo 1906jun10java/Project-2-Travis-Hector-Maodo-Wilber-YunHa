36ad14b88d148ba49bbd1080b8fc0859470a68b3
travywill <travywill@gmail.com>
2019-08-09 18:14:25 -0400

    adds complete purchase method

--- p2/src/main/java/com/revature/services/PurchaseService.java
+++ p2/src/main/java/com/revature/services/PurchaseService.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,21 +14,16 @@ import com.revature.daos.*;
 public class PurchaseService {
 
 	private PurchaseDAO purchaseDAO;
+	private ProductDAO productDAO;
 	
 	@Autowired
 	public PurchaseService(PurchaseDAO purchaseDAO) {
 		this.purchaseDAO = purchaseDAO;
 	}
 	
-	/*
-	 * public void addPurchase(Parsing data) { System.out.println(data); User tuser
-	 * = new User(data.getUserId(), data.getFirstName(), data.getLastName(),
-	 * data.getEmail(), null); Purchase tpurchase = new Purchase(); //Payment
-	 * tpayment = new Payment(data.getInteger(), data.getInteger(),
-	 * data.getString(), data.getDate());
-	 * 
-	 * //this.purchaseDAO.addPurchase(tpurchase, tpayment); }
-	 */
+	public void addPurchase(Purchase purchase) {
+		this.purchaseDAO.addPurchase(purchase);
+	}
 	
 	public List<Purchase> getAllPurchases() {
 		return this.purchaseDAO.getAllPurchases();
@@ -38,5 +35,30 @@ public class PurchaseService {
 	
 	public List<Purchase> getPurchasesForUser(int userId) {
 		return this.purchaseDAO.getPurchasesForUser(userId);
+	}
+	
+	public void completePurchase(List<Object> cartItems) {
+		System.out.println(cartItems);
+		User user = (User) cartItems.get(0);
+		System.out.println(user);
+		Double total = (Double) cartItems.get(1);
+		System.out.println(total);
+		Payment payment = (Payment) cartItems.get(2);
+		System.out.println(payment);
+		Address address = (Address) cartItems.get(3);
+		System.out.println(address);
+		Date date = (Date) cartItems.get(4);
+		System.out.println(date);
+		
+		Purchase purchase = new Purchase(user, total, payment, address, date);
+		System.out.println(purchase);
+		purchaseDAO.addPurchase(purchase);
+		
+		List<Product> purchaseItems = (List<Product>) cartItems.get(5);
+		
+		for (Product product : purchaseItems) {
+			productDAO.addProduct(product);
+			System.out.println(product);
+		}
 	}
 }
